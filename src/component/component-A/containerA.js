49cd223aa79f6d2d5be06bcadaa6e2617a36315a
Adan Rodriguez <Adanr760@gmail.com>
2022-11-10 19:26:37 -0300

    a

--- src/component/component-A/containerA.js
+++ src/component/component-A/containerA.js
@@ -5,7 +5,10 @@ const ContainerA = () => {
 	const c2 = new contacto("joe", "rodriguez", "adaadsad@gmail.com", false);
 	const contatos = [c1, c2];
 	return (
-		<div>
+		<div
+			style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+		>
+			<h2>Contactos</h2>
 			{contatos.map((c, i) => {
 				return <ContainerB {...c} id={i + 1} key={i} />;
 			})}
